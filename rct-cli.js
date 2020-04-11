4c58419da4f506e2a434900a568b8b42343c30ea
Chris Emerson <cme210@yahoo.com>
2020-04-11 09:09:59 -0400

    updates

--- rct-cli.js
+++ rct-cli.js
@@ -39,33 +39,27 @@ var setMethods = [
   //'eyes.addProperty("PROPERTY", "PROP-VALUE")'
 ];
 
-// Methods
-// 1) setup the common eyes properties for grid or classic runner (API key, stitch mode, match level, batch id)
-// 2) setup the branch names separately to be able eto play around with them
-// 3) eyes run caller that takes a url and vg or classic flag, and a baseline identifier and runs a test with a new eyes instance each time
-
-// Questions
-// 1) Use environments and the order the tests are compared to match baselines to checkpoints (I could have a linux machine that 
-// has the same environment as the VG but if I run it second it matches my machine against the grid baseline)
-// 2) OR use baseline branch names and force the matches to take place with a simple branch name (but environment could differ and then I'm back to 1)
-
-async function getBrowser() {
+function getBrowser() {
 
     // Get a browser
     // TODO: Add Sauce, Browserstack, Perfecto, Plain Selenium grid
 
-    var options = new chrome.Options();
-       // options.addArguments('--headless');
-        options.addArguments('disable-gpu');
-        options.addArguments('disable-infobars');
-    
-    let driver = new webdriver.Builder()
-        .forBrowser('chrome')
-        .withCapabilities(webdriver.Capabilities.chrome())
-        .setChromeOptions(options)          
-        .build();
+    try{
+        var options = new chrome.Options();
+        // options.addArguments('--headless');
+            options.addArguments('disable-gpu');
+            options.addArguments('disable-infobars');
+        
+        let driver = new webdriver.Builder()
+            .forBrowser('chrome')
+            .withCapabilities(webdriver.Capabilities.chrome())
+            .setChromeOptions(options)          
+            .build();
 
-    return driver;
+        return driver;
+    } catch(err) {
+        console.error('ERROR: function getBrowser() : ' + err.message);
+    }
 }
 
 async function eyesSetup(eyesConfig) {
@@ -75,10 +69,9 @@ async function eyesSetup(eyesConfig) {
         let eyes = new Eyes(runner);
 
         eyes.setApiKey(eyesConfig.apiKey);
-        eyes.setLogHandler(new ConsoleLogHandler(false));
+        if(eyesConfig.log){eyes.setLogHandler(new ConsoleLogHandler(false));}        
 
         for (let i = 0; i < setMethods.length; i++) {
-            console.log(setMethods[i]);
             try { 
                 await eval(setMethods[i]);
             } catch(err) {
@@ -90,62 +83,70 @@ async function eyesSetup(eyesConfig) {
         batchInfo.setId(eyesConfig.batchId);
 
         if(eyesConfig.useGrid) {
-
             const configuration = new Configuration();
             configuration.setBatch(batchInfo)
             configuration.setConcurrentSessions(5);
             configuration.setAppName(eyesConfig.appName);
             configuration.setTestName(eyesConfig.testName);
-            configuration.setMatchLevel('Layout');
+            configuration.setMatchLevel('Strict');
+            configuration.setSaveFailedTests(true);
             configuration.addBrowser(eyesConfig.vx,  eyesConfig.vy, BrowserType.CHROME);
-            await eyes.setConfiguration(configuration);
-            return eyes;
+            eyes.setConfiguration(configuration);
+        } else {
+            eyes.setBatch(batchInfo);
         }
+        return eyes;
     } catch(err) {
-        console.error(err.message);
+        console.error('ERROR: function eyesConfig() : ' +err.message);
     } 
 
 }
 
-async function runEyes(rdriver, reyes, eyesConfig) {
+async function runEyes(reyes, eyesConfig) {
+
+    const driver = await getBrowser()
 
     try {
-
         if(eyesConfig.useGrid){
-            await reyes.open(rdriver);
+            await reyes.open(driver);
         } else {
-            await reyes.open(rdriver, eyesConfig.appName, eyesConfig.testName, { width: eyesConfig.vx, height: eyesConfig.vy });
+            await reyes.open(driver, eyesConfig.appName, eyesConfig.testName, { width: eyesConfig.vx, height: eyesConfig.vy });
         }
-
         // await rdriver.get('https://www.timeanddate.com/worldclock/usa/melbourne');  // causes JSON error !!!! Stringify of an element
         // await rdriver.get('https://applitools.github.io/demo/TestPages/FramesTestPage');
-        await rdriver.get('https://www.random.org/integers/?num=100&min=1&max=100&col=5&base=10&format=html&rnd=new');
-
-        
-        var currentUrl = await rdriver.getCurrentUrl().then(function(url){
+        await driver.get(eyesConfig.url);
+        var currentUrl = await driver.getCurrentUrl().then(function(url){
             console.log('Testing URL: ' + url);
             return url;
-            });
-
+        });
         await reyes.check(currentUrl, Target.window().fully());
-        await reyes.close(false);
-        const results = await reyes.getRunner().getAllTestResults();
+        if(eyesConfig.useGrid) { await reyes.closeAsync() } else { await reyes.close(false) }
+        const results = await reyes.getRunner().getAllTestResults(false);
         console.log(results); 
 
-        await rdriver.quit();
-    
     } catch(err) {
 
-        console.error(err.message);
-        
-        if (rdriver && reyes) {
-            await rdriver.quit();
-            await reyes.abortIfNotClosed();
-        }
+        console.error('ERROR: function runEyes() : ' + err.message);
 
-    } 
+    } finally {
+        if (driver && reyes) {
+            driver.quit();
+            reyes.abortIfNotClosed();
+        }
+    }
 }
 
+function logEyesConfig(eyesConfig){
+    l('log eyesConfig  start')
+    Object.getOwnPropertyNames(eyesConfig).forEach(
+        function (val, idx, array) {
+          console.log(val + ': ' + eyesConfig[val])
+        }
+      );
+      l('log eyesConfig end')
+}
+
+function l(msg){ console.log(new Date() + ': ' + msg)}
 
 ;(async function() {
 
@@ -155,13 +156,13 @@ async function runEyes(rdriver, reyes, eyesConfig) {
         .version('0.0.1', '-v, --version', 'output the current version')
         .description('A tool for comparing UFG and Classic renders on various platforms')
         .option('-k --key [key]', 'Set your Applitools API Key. e.g. -k key', process.env.APPLITOOLS_API_KEY)
-        .option('-u --url [url]', 'Add the site URL you want to generate a sitemap for. e.g. -u https://www.applitools.com')
+        .option('-u --url [url]', 'Add the site URL you want to generate a sitemap for. e.g. -u https://www.applitools.com', 'https://www.random.org/integers/?num=100&min=1&max=100&col=5&base=10&format=html&rnd=new')
         .option('-sk --saucekey [saucekey]', 'Your Saucelabs key. Default: local headless chromedriver')
         .option('-bn --batchName [batchName]', 'Name for the final comparison batch', 'rct batch')
         .option('-vx --xdim [xdim]', 'X dimension of the viewport size. e.g. -vx 1600', 1600)
         .option('-vy --ydim [ydim]', 'Y dimension of the viewport size. e.g. -vy 900', 900)
         .option('-su  --serverUrl [serverUrl]', 'Set your Applitools  server URL. (Default: https://eyesapi.applitools.com). e.g. -v https://youreyesapi.applitools.com', 'https://eyesapi.applitools.com')
-        .option('-l --log', 'Enable Applitools Debug Logs (Default: false). e.g. --log')
+        .option('-l --log [log]', 'Enable Applitools Debug Logs (Default: false). e.g. --log', false)
         .option('-an --appName [appName]', 'Name of the application under test', 'rct app')
         .option('-tn --testName [testName]', 'The name of the final comparison test in the Applitools batch', 'rct test')
         .option('-sm --stitchMode [stitchMode]', 'The stitchmode to be used (Default: CSS)', 'CSS')
@@ -172,8 +173,7 @@ async function runEyes(rdriver, reyes, eyesConfig) {
         })
         .parse(process.argv);
 
-    var eyesConfig = {
-        useGrid: true,
+    const eyesConfig = {
         vx: program.xdim,
         vy: program.ydim,
         batchName: program.batchName,
@@ -183,38 +183,34 @@ async function runEyes(rdriver, reyes, eyesConfig) {
         appName: program.appName,
         testName: program.testName,
         serverUrl: program.serverUrl,
-        stitchMode: program.stitchMode
+        stitchMode: program.stitchMode,
+        log: program.log
     }
-
-    Object.getOwnPropertyNames(eyesConfig).forEach(
-        function (val, idx, array) {
-          console.log(val + ': ' + eyesConfig[val]);
-        }
-      );
 
     try {
 
-        let eyes1 = await eyesSetup(eyesConfig);
-        let driver = await getBrowser();
-        await runEyes(driver, eyes1, eyesConfig);
-        
-        eyesConfig.useGrid = false;
-        console.log('-------------2');
-        let eyes2 = await eyesSetup();
-        console.log('-------------2');
-        console.log('-------------3');
-        await runEyes(driver, eyes2, eyesConfig);  
-        console.log('-------------3');
+        logEyesConfig(eyesConfig)
+
+        eyesConfig.useGrid = true
+        const eyesVisualGrid = await eyesSetup(eyesConfig)
+
+        l('Grid run begin')
+        await runEyes(eyesVisualGrid, eyesConfig);
+        l('Grid run end')
+
+        eyesConfig.useGrid = false
+        const eyesClassic = await eyesSetup(eyesConfig)
+
+        l('Classic run begin')
+        await runEyes(eyesClassic, eyesConfig);  
+        l('Classic run end')  
+
+        logEyesConfig(eyesConfig)
 
     } catch(err) {
-
         console.error(err.message);
-    
-        if (driver && eyes) {
-            await rdriver.quit();
-            await reyes.abortIfNotClosed();
-        }
-
+        if(eyesVisualGrid) eyesVisualGrid.abortIfNotClosed();
+        if(eyesClassic) eyesClassic.abortIfNotClosed();
     }
 
 })()
